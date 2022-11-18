10ec86a4b51eac34f0acfb04a8004e20ddfeb911
Thomas Walter <speisekatze@gmail.com>
2022-11-18 17:15:22 +0100

    using file from config

--- src/db.py
+++ src/db.py
@@ -1,7 +1,9 @@
 import sqlite3
+import src.config as toolbus
+
 
 def get_group(name):
-    con = sqlite3.connect('inventory/hosts.sqlite')
+    con = sqlite3.connect(toolbus.db['filename'])
     cur = con.cursor()
     r = cur.execute(f"SELECT * FROM groups WHERE name = '{name}'")
     group_info = r.fetchone()
@@ -9,7 +11,7 @@ def get_group(name):
     return group_info
 
 def get_host(uid):
-    con = sqlite3.connect('inventory/hosts.sqlite')
+    con = sqlite3.connect(toolbus.db['filename'])
     cur = con.cursor()
     r = cur.execute(f"SELECT * FROM hosts WHERE uid = '{uid}'")
     host_info = r.fetchone()
@@ -26,7 +28,7 @@ def get_host(uid):
 
 def get_next_stage(stage):
     stage += 1
-    con = sqlite3.connect('inventory/hosts.sqlite')
+    con = sqlite3.connect(toolbus.db['filename'])
     cur = con.cursor()
     r = cur.execute(f"SELECT * FROM flow WHERE sort = {stage}")
     flow_info = r.fetchone()
@@ -37,28 +39,28 @@ def get_next_stage(stage):
     return flow_info
 
 def update_group(id, serial):
-    con = sqlite3.connect('inventory/hosts.sqlite')
+    con = sqlite3.connect(toolbus.db['filename'])
     cur = con.cursor()
     cur.execute(f"UPDATE groups SET serial = {serial} WHERE id = {id}")
     con.commit()
     con.close()
 
 def update_stage(host):
-    con = sqlite3.connect('inventory/hosts.sqlite')
+    con = sqlite3.connect(toolbus.db['filename'])
     cur = con.cursor()
     cur.execute(f"UPDATE hosts SET stage = {host['stage']} WHERE uid = '{host['uid']}'")
     con.commit()
     con.close()
 
 def new_host(host):
-    con = sqlite3.connect('inventory/hosts.sqlite')
+    con = sqlite3.connect(toolbus.db['filename'])
     cur = con.cursor()
     cur.execute(f"INSERT INTO hosts(uid, `group`, ip, hostname, stage) VALUES('{host['uid']}', {host['group']}, '{host['ip']}', '{host['hostname']}', {host['stage']} )")
     con.commit()
     con.close()
 
 def get_ips_from_group(groupid):
-    con = sqlite3.connect('inventory/hosts.sqlite')
+    con = sqlite3.connect(toolbus.db['filename'])
     cur = con.cursor()
     r = cur.execute(f"SELECT ip FROM hosts WHERE `group` = {groupid}")
     ips = r.fetchall()
