0ae3b2eff379d1e2905ba00b21992955ec21a3fb
Thomas Walter <speisekatze@gmail.com>
2022-12-03 14:13:12 +0100

    script for hostname request

---
+++ src/paths/hostname.py
@@ -0,0 +1,13 @@
+import src.config as toolbus
+import src.helper as helper
+import src.db as db
+
+
+def execute(message):
+    print('loaded Hostname')
+    host_info = prepare_request(message)
+    host_info['stage'] = 2
+    db.update_stage(host_info)
+    options = {'hostname': host_info['hostname']}
+    return helper.prepare_result(host_info, 3, options=options)
+
