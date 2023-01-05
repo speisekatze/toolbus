1e2a10b1ded83ad4ed336c972d1a7b72c53d2740
Thomas Walter <speisekatze@gmail.com>
2023-01-05 06:34:59 +0100

    added stage ssh

---
+++ src/paths/ssh.py
@@ -0,0 +1,12 @@
+import src.config as toolbus
+import src.helper as helper
+import src.db as db
+
+
+def execute(message):
+    print('loaded ssh')
+    host_info = helper.prepare_request(message)
+    host_info['stage'] = 3
+    db.update_stage(host_info)
+    return helper.prepare_result(host_info, 3, payload=toolbus.cluster['users'], options={})
+
